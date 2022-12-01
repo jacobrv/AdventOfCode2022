3d578b770f67902e96d3e157f7a83d42e4e22523
VARNS Jacob JJ5660 <JJ5660@engie.com>
2022-12-01 09:45:44 -0800

    day 1

---
+++ Day01/Part2.py
@@ -0,0 +1,49 @@
+#with open('sample_input.txt', 'r') as f:
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+
+
+elves = []
+bags = []
+
+for line in lines:
+    line = line.strip()
+    if(line == ''):
+        elves.append(bags.copy())
+        bags = []
+    else:
+        bags.append(line)
+
+if(bags.__len__() > 0):
+    elves.append(bags.copy())
+    bags = []
+
+
+#print(elves)
+
+highIndex = -1
+highTotal = -1
+
+elfTotals = []
+
+for idx, elf in enumerate(elves):
+    total = 0
+    for bag in elf:
+        total = total + int(bag)
+    if(total > highTotal):
+        highTotal = total
+        highIndex = idx
+    elfTotals.append(total)
+
+print(highIndex)
+print(highTotal)
+
+elfTotals.sort()
+
+topThreeTotal = 0
+topThreeTotal = topThreeTotal + elfTotals.pop()
+topThreeTotal = topThreeTotal + elfTotals.pop()
+topThreeTotal = topThreeTotal + elfTotals.pop()
+
+print(topThreeTotal)
+
