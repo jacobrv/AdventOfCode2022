df4ba959270fb1bc24e3ba021290f990698b9703
VARNS Jacob JJ5660 <JJ5660@engie.com>
2022-12-05 10:50:32 -0800

    day 4

---
+++ Day04/Part2.py
@@ -0,0 +1,40 @@
+#with open('sample_input.txt', 'r') as f:
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+    
+
+bags = []
+
+for line in lines:
+    line = line.strip()
+    if(line == ''):
+        pass
+    else:
+        pair = line.split(',')
+        s1 = pair[0].split('-')
+        s2 = pair[1].split('-')
+        bags.append([s1, s2])
+        
+print(bags)
+
+numContained = 0
+
+for bag in bags:
+    if(int(bag[0][0]) >= int(bag[1][0]) and int(bag[0][1]) <= int(bag[1][1])):
+        numContained += 1
+        print(bag)
+        continue
+    if(int(bag[1][0]) >= int(bag[0][0]) and int(bag[1][1]) <= int(bag[0][1])):
+        numContained += 1
+        print(bag)
+        continue
+    if(int(bag[0][0]) >= int(bag[1][0]) and int(bag[0][0]) <= int(bag[1][1])):
+        numContained += 1
+        print(bag)
+        continue
+    if(int(bag[0][1]) >= int(bag[1][0]) and int(bag[0][1]) <= int(bag[1][1])):
+        numContained += 1
+        print(bag)
+        continue
+    
+print(numContained)
