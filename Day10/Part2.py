a02251692c1c2bc7a75fc69c07fe857c265eb458
VARNS Jacob JJ5660 <JJ5660@engie.com>
2022-12-12 16:55:28 -0800

    day 10

---
+++ Day10/Part2.py
@@ -0,0 +1,99 @@
+import math
+
+#with open('sample_input.txt', 'r') as f:
+#with open('sample_input copy.txt', 'r') as f:
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+
+instMaxCycleTime = 3
+
+instructions = []
+
+for line in lines:
+    tLine = line.strip()
+    if tLine == 'noop':
+        instructions.append([tLine, '0'])
+    else:    
+        aLine = tLine.split(' ')
+        aLine[1] = int(aLine[1])
+        instructions.append(aLine)    
+
+regAtT = [1]
+reg = 1
+queuedAdds = [0]*instMaxCycleTime
+currentCycle = 0
+
+def dequeueAdd() -> int:
+    addToReturn = queuedAdds[0]
+    for i in range(instMaxCycleTime-1):
+        queuedAdds[i] = queuedAdds[i+1]
+    queuedAdds[instMaxCycleTime-1] = 0
+    return addToReturn
+    
+def enqueueAdd(num, cycleTime):
+    queuedAdds[cycleTime] += num
+    
+    
+for inst in instructions:
+    if inst[0] == 'noop':
+        enqueueAdd( 0, 1)
+        
+        n = dequeueAdd()
+        reg += n
+        currentCycle += 1
+        regAtT.append(reg)
+        
+        print(queuedAdds)
+        print(reg)
+        
+    if inst[0] == 'addx':
+        enqueueAdd( inst[1], 1)
+        enqueueAdd( 0, 1)
+        
+        n = dequeueAdd()
+        reg += n
+        currentCycle += 1
+        regAtT.append(reg)
+        
+        print(queuedAdds)
+        print(reg)
+        
+        n = dequeueAdd()
+        reg += n
+        currentCycle += 1
+        regAtT.append(reg)
+        
+        print(queuedAdds)
+        print(reg)
+    
+print('*')
+print(regAtT[139])
+print(regAtT[140])
+print(regAtT[141])
+print('*')
+print(regAtT[179])
+print(regAtT[180])
+print(regAtT[181])
+print('*')
+print(regAtT[219])
+print(regAtT[220])
+print(regAtT[221])
+print('*')
+
+
+scanlines = []
+
+
+for i in range(1, 241, 40):
+    sl = ''
+    
+    for j in range(0, 40):
+        vis = '.'
+        if regAtT[i + j - 1] <= j+1 and regAtT[i + j - 1] >= j-1:
+            vis = '#'
+        sl = sl + vis
+    
+    scanlines.append(sl)
+    
+for sl in scanlines:
+    print(sl)
